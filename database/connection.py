9bada421e30a8b00f3b744ccfee32ebbe3eb0250
AdItyAsIngh1800 <aditya.si181@gmail.com>
2026-03-21 03:12:24 +0530

    Updated connections.py

--- database/connection.py
+++ database/connection.py
@@ -1,4 +1,6 @@
 import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +10,14 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        cursor_factory=RealDictCursor
+    )
